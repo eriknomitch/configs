49b268b1b0dce5ca91d3af6473fa5bed6b74c349
Erik Nomitch <enomitch@gmail.com>
2016-02-03 07:16:01 -0600

    suffix color switch

--- zshrc-group-specific/micro.zsh
+++ zshrc-group-specific/micro.zsh
@@ -1,7 +1,12 @@
 _suffix_color=`micro-hostname-suffix`
 
-if [[ $_suffix_color == "black" ]] ; then
-  _suffix_color="white"
-fi
+case $_suffix_color in
+  black)
+    _suffix_color="white"
+    ;;
+  orange)
+    _suffix_color="red"
+    ;;
+esac
 
 PROMPT=$'->%{$fg_bold[blue]%}[SSH]%{$reset_color%}-[%{$fg_bold[white]%}$(pwd-is-wd-character)%{$reset_color%}]$(pwd-home-count)->%{$fg[magenta]%}micro-%{$reset_color%}%{$fg[magenta]%}%{$fg[$_suffix_color]%}$%{$reset_color%} '
