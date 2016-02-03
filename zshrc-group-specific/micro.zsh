20f93f9f2fced8fa30bb6dc15e5e76d6f4687d27
Erik Nomitch <enomitch@gmail.com>
2016-02-03 07:10:33 -0600

    PROMPT in micro.zsh

--- zshrc-group-specific/micro.zsh
+++ zshrc-group-specific/micro.zsh
@@ -1,1 +1,7 @@
-echo ok
+_suffix_color=`micro-hostname-suffix`
+
+if [[ $_suffix_color == "black" ]] ; then
+  _suffix_color="white"
+fi
+
+PROMPT=$'->%{$fg_bold[blue]%}[SSH]%{$reset_color%}-[%{$fg_bold[white]%}$(pwd-is-wd-character)%{$reset_color%}]$(pwd-home-count)->%{$fg[magenta]%}micro-%{$reset_color%}%{$fg[magenta]%}%{$fg[$_suffix_color]%}$%{$reset_color%} '
