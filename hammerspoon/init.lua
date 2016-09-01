c464bee10c412256cf0f0c560347d63b06640677
Erik Nomitch <enomitch@gmail.com>
2016-09-01 13:11:24 -0500

    A hammerspoon/init.lua

---
+++ hammerspoon/init.lua
@@ -0,0 +1,65 @@
+--hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  --hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+--end)
+
+local movement = {"cmd", "ctrl"}
+local hyper    = {"shift", "cmd", "alt", "ctrl"}
+
+hs.window.animationDuration = 0
+
+hs.hotkey.bind(movement, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(movement, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+
+hs.hotkey.bind(movement, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-----------------------------------------------
+-- Reload config on write
+-----------------------------------------------
+function reload_config(files)
+  hs.reload()
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.alert.show("Config loaded")
+
+-----------------------------------------------
+-- Hyper i to show window hints
+-----------------------------------------------
+
+hs.hotkey.bind(movement, "h", function()
+  hs.hints.windowHints()
+end)
