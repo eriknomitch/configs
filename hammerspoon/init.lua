18c14988ab408c64ad9770652937c0e49c0ad7ca
Erik Nomitch <enomitch@gmail.com>
2016-09-08 09:35:09 -0500

    .

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -54,7 +54,6 @@ function reload_config(files)
   hs.reload()
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
-hs.alert.show("Config loaded")
 
 -----------------------------------------------
 -- Hyper i to show window hints
@@ -63,3 +62,122 @@ hs.alert.show("Config loaded")
 hs.hotkey.bind(movement, "h", function()
   hs.hints.windowHints()
 end)
+
+
+local layouts = {
+  {
+    name = {"Kiwi for Gmail"},
+    func = function(index, win)
+      win:maximize()
+    end
+  }
+}
+
+function watchWindow(win, initializing)
+  hs.alert.show("Window")
+  --local appWindows = watchers[win:application():pid()].windows
+  --if win:isStandard() and not appWindows[win:id()] then
+    --local watcher = win:newWatcher(handleWindowEvent, {pid=win:pid(), id=win:id()})
+    --appWindows[win:id()] = watcher
+
+    --watcher:start({events.elementDestroyed, events.windowResized, events.windowMoved})
+
+    --if not initializing then
+      --hs.alert.show('window created: '..win:id()..' with title: '..win:title())
+    --end
+  --end
+end
+
+function handleAppEvent(element, event)
+
+  if event == events.windowCreated then
+    watchWindow(element)
+  elseif event == events.focusedWindowChanged then
+    -- Handle window change
+  end
+end
+
+function handleGlobalAppEvent(name, event, app)
+  if event == hs.application.watcher.launched then
+    hs.alert.show("App launched")
+    local watcher = app:newWatcher(handleAppEvent)
+    watcher:start({events.windowCreated, events.focusedWindowChanged})
+  end
+end
+
+
+local watcher = hs.application.watcher.new(handleGlobalAppEvent)
+watcher:start()
+
+
+hs.alert.show("Config loaded")
+
+
+--local events = hs.uielement.watcher
+
+--watchers = {}
+
+--function init()
+  --appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+  --appsWatcher:start()
+
+  ------ Watch any apps that already exist
+  ----local apps = hs.application.runningApplications()
+  ----for i = 1, #apps do
+    ----if apps[i]:title() ~= "Hammerspoon" then
+      ----watchApp(apps[i], true)
+    ----end
+  ----end
+--end
+
+--function handleGlobalAppEvent(name, event, app)
+  --if     event == hs.application.watcher.launched then
+    --watchApp(app)
+  --elseif event == hs.application.watcher.terminated then
+    ---- Clean up
+    --local appWatcher = watchers[app:pid()]
+    --if appWatcher then
+      --appWatcher.watcher:stop()
+      --for id, watcher in pairs(appWatcher.windows) do
+        --watcher:stop()
+      --end
+      --watchers[app:pid()] = nil
+    --end
+  --end
+--end
+
+--function watchApp(app, initializing)
+  --if watchers[app:pid()] then return end
+
+  --local watcher = app:newWatcher(handleAppEvent)
+  --watchers[app:pid()] = {watcher = watcher, windows = {}}
+
+  --watcher:start({events.windowCreated, events.focusedWindowChanged})
+
+  ---- Watch any windows that already exist
+  --for i, window in pairs(app:allWindows()) do
+    --watchWindow(window, initializing)
+  --end
+--end
+
+--function handleAppEvent(element, event)
+  --if event == events.windowCreated then
+    --watchWindow(element)
+  --elseif event == events.focusedWindowChanged then
+    ---- Handle window change
+  --end
+--end
+
+
+--function handleWindowEvent(win, event, watcher, info)
+  --if event == events.elementDestroyed then
+    --watcher:stop()
+    --watchers[info.pid].windows[info.id] = nil
+  --else
+    ---- Handle other events...
+  --end
+  --hs.alert.show('window event '..event..' on '..info.id)
+--end
+
+--init()
+
