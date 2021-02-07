3e50dd099f2b7fb82b30628baf4d7f2bacccd224
Erik Nomitch <enomitch@gmail.com>
2021-02-07 14:07:34 -0600

    .

--- zshrc-zplug-init.sh
+++ zshrc-zplug-init.sh
@@ -12,21 +12,18 @@ zplug "plugins/command-not-found", from:oh-my-zsh
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-completions"
 
-# ------------------------------------------------
-zplug "psprint/zsh-navigation-tools"
-zplug "mafredri/zsh-async"
-zplug "sindresorhus/pure"
-
-# rupa/z
-# jocelynmallon/zshmarks
-
-# ------------------------------------------------
-
 # Set the priority when loading
 # e.g., zsh-syntax-highlighting must be loaded
 # after executing compinit command and sourcing other plugins
 # (If the defer tag is given 2 or above, run after compinit command)
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
+# ------------------------------------------------
+zplug "psprint/zsh-navigation-tools"
+zplug "mafredri/zsh-async"
+zplug "sindresorhus/pure"
+zplug "jocelynmallon/zshmarks"
+zplug "rupa/z"
 
 # ------------------------------------------------
 # ------------------------------------------------
