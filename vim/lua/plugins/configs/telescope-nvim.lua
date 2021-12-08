96919193d18d45c2fbe3f4fcf0fc5a9667b9aec3
Erik Nomitch <enomitch@gmail.com>
2021-12-08 10:50:33 -0600

    Map live_grep

--- vim/lua/plugins/configs/telescope-nvim.lua
+++ vim/lua/plugins/configs/telescope-nvim.lua
@@ -10,5 +10,5 @@
 
 nnoremap({"<Leader>fb", "<Leader>b" }, ":lua require('telescope.builtin').buffers()<Cr>", "silent")
 nnoremap({"<Leader>ff", "<Leader>f" }, ":lua require('telescope.builtin').find_files()<Cr>", "silent")
-nnoremap({"<Leader>fg"}, ":lua require('telescope.builtin').live_grep()<Cr>", "silent")
+nnoremap({"<Leader>fg", "<Leader>g"}, ":lua require('telescope.builtin').live_grep()<Cr>", "silent")
 
