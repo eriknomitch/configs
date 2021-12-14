4470ddcb2d5524fdc163cd0eeb49f25f91209155
Erik Nomitch <enomitch@gmail.com>
2021-12-14 14:45:53 -0600

    Add example lua

--- vim/lua/plugins/init.lua
+++ vim/lua/plugins/init.lua
@@ -10,13 +10,35 @@ require("plugins.configs.nvim-cmp")
 require("plugins.configs.telescope-nvim")
 require("plugins.configs.coc-nvim")
 require("plugins.configs.trouble")
+require("plugins.configs.nvim-luadev")
 
-local plugin_configs_path = vim.fn.stdpath('data') ..'/site/pack/packer/opt/packer.nvim'
-
--- local files = vim.fn.glob('lua/plugins/configs/*.lua')
-
--- print(files[0])
+-- Require all .lua files in the plugins/configs directory
+-- local files = vim.split(vim.fn.glob('plugins/configs/*.lua'), "\n")
 
 -- for _, file in ipairs(files) do
---   print(file)
+--   -- print(file:gsub('^plugins/configs/', ''):gsub('%.lua$', ''))
+--   -- vim.api.nvim_echo(file)
+--   vim.cmd(string.format("source %s", file))
 -- end
+
+-- vim.g.
+-- vim.fn.expand
+
+-- print(vim.fn.expand('%:.'))
+
+
+
+-- local glob = vim.fn.stdpath('data') .. '/lua/plugins/configs/*.lua'
+
+-- local files = vim.split(vim.fn.glob(glob), "\n")
+
+-- for _, file in ipairs(files) do
+--   require(file)
+-- end
+
+
+
+-- local plugin_configs_path = vim.fn.stdpath('data') ..'/site/pack/packer/opt/packer.nvim'
+-- local files = vim.fn.glob('lua/plugins/configs/*.lua')
+-- print(files[0])
+
