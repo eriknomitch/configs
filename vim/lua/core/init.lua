2d93f3d30ec72363c82ebda28d93523d5b857567
Erik Nomitch <enomitch@gmail.com>
2021-12-14 04:11:50 -0600

    Fix core

--- vim/lua/core/init.lua
+++ vim/lua/core/init.lua
@@ -2,3 +2,6 @@ local core_modules = {
    "core.utilities",
    "core.mappings",
 }
+
+require("core.utilities")
+require("core.mappings")
--- vim/lua/core/mappings.lua
+++ vim/lua/core/mappings.lua
@@ -6,3 +6,6 @@
 nnoremap("<Leader>sv", ":source $MYVIMRC | echon '\"'$MYVIMRC'\" sourced'<CR>")
 nnoremap("<Leader>so", ":source % | echon '\"'expand('%')'\" sourced'<CR>")
 nnoremap("<Leader>M", ":messages<CR>")
+
+nnoremap("<Leader>vs", ":vsplit<Cr>", "silent")
+nnoremap("<Leader>sp", ":split<Cr>", "silent")
