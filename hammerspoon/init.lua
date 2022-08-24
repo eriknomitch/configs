936a7b2bdcc0ea800a925f3fc17cad2a194765e8
Erik Nomitch <enomitch@gmail.com>
2022-08-24 14:14:58 -0500

    .

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -518,6 +518,7 @@ hs.hotkey.bind({"cmd", "ctrl", "shift"}, "'", function()switcher:previous()end)
 -- -----------------------------------------------
 -- HOTKEYS->VOLUME -------------------------------
 -- -----------------------------------------------
+--{{{
 hs.hotkey.bind({"shift", "cmd", "ctrl"}, "Up", function()
   setVolumeOfCurrent(10.0)
 end)
@@ -525,6 +526,7 @@ end)
 hs.hotkey.bind({"shift", "cmd", "ctrl"}, "Down", function()
   setVolumeOfCurrent(-10.0)
 end)
+--}}}
 
 -- -----------------------------------------------
 -- HOTKEYS->WINDOW -------------------------------
