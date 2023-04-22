7ce6204c383824997aa5b3622b6cc53d2792345f
Erik Nomitch <enomitch@gmail.com>
2023-04-22 05:33:41 -0500

    .

--- iterm2.zsh
+++ iterm2.zsh
@@ -6,15 +6,15 @@
 # iTerm2 window/tab color commands
 #   Requires iTerm2 >= Build 1.0.0.20110804
 #   http://code.google.com/p/iterm2/wiki/ProprietaryEscapeCodes
-# tab-color() {
-#     echo -ne "\033]6;1;bg;red;brightness;$1\a"
-#     echo -ne "\033]6;1;bg;green;brightness;$2\a"
-#     echo -ne "\033]6;1;bg;blue;brightness;$3\a"
-# }
-# tab-reset() {
-#     echo -ne "\033]6;1;bg;*;default\a"
-#     trap - INT EXIT
-# }
+tab-color() {
+    echo -ne "\033]6;1;bg;red;brightness;$1\a"
+    echo -ne "\033]6;1;bg;green;brightness;$2\a"
+    echo -ne "\033]6;1;bg;blue;brightness;$3\a"
+}
+tab-reset() {
+    echo -ne "\033]6;1;bg;*;default\a"
+    trap - INT EXIT
+}
 
 # # Change the color of the tab when using SSH
 # # reset the color after the connection closes
@@ -34,8 +34,8 @@
 # alias ssh="color-ssh"
 
 # # Default tab color
-# tab-color 255 255 255
-# tab-color 0 0 0
+tab-color 255 255 255
+tab-color 0 0 0
 
 
 # ================================================================
