e431cbba7f39c8eee4dc881007ded40ea972c53c
Erik Nomitch <enomitch@gmail.com>
2023-06-07 20:35:06 -0500

    _d

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -147,10 +147,10 @@ null_ls.setup({
         debug = true,
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.eslint,
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.completion.spell,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.eslint_d,
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.completion.spell
 	},
 })
 
