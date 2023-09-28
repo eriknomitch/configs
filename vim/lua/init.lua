86d21bd6e2680c988ca8643150cc3508c95ff94e
Erik Nomitch <enomitch@gmail.com>
2023-09-28 18:23:41 -0500

    .

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -205,21 +205,21 @@ vim.opt.termguicolors = true
 vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]])
 
-require("indent_blankline").setup({
-	char = "",
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-	},
-	space_char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-	},
-	show_trailing_blankline_indent = false,
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
-})
+-- require("indent_blankline").setup({
+-- 	char = "",
+-- 	char_highlight_list = {
+-- 		"IndentBlanklineIndent1",
+-- 		"IndentBlanklineIndent2",
+-- 	},
+-- 	space_char_highlight_list = {
+-- 		"IndentBlanklineIndent1",
+-- 		"IndentBlanklineIndent2",
+-- 	},
+-- 	show_trailing_blankline_indent = false,
+-- 	space_char_blankline = " ",
+-- 	show_current_context = true,
+-- 	show_current_context_start = true,
+-- })
 
 -- --------------------------------------
 -- --------------------------------------
