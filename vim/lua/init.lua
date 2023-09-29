7930fb676ffcc7774b52de873cfcfca77bd20376
Erik Nomitch <enomitch@gmail.com>
2023-09-29 03:29:16 -0500

    lines

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -197,13 +197,13 @@ require("nvim-autopairs").setup()
 
 require("gitsigns").setup()
 
-vim.opt.list = true
-vim.opt.listchars:append("space: ")
-vim.opt.listchars:append("eol:↴")
-
-vim.opt.termguicolors = true
-vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]])
+-- vim.opt.list = true
+-- vim.opt.listchars:append("space: ")
+-- vim.opt.listchars:append("eol:↴")
+--
+-- vim.opt.termguicolors = true
+-- vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]])
+-- vim.cmd([[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]])
 
 -- require("indent_blankline").setup({
 -- 	char = "",
