9ef3030749ed1ec9719c65abe66a82444f850df9
Erik Nomitch <enomitch@gmail.com>
2024-05-31 17:01:21 -0500

    .

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -203,7 +203,8 @@ require("lazy").setup({
 		config = function()
 			require("neoscroll").setup({
 				-- All these keys will be mapped to their corresponding default scrolling animation
-				mappings = { "<Space>", "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+				-- mappings = { "<Space>", "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+				mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
 				hide_cursor = true, -- Hide cursor while scrolling
 				stop_eof = true, -- Stop at <EOF> when scrolling downwards
 				respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
