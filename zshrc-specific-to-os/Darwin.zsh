a53b1cb4d0efab2b5038d9cb50801dcc36118e51
Erik Nomitch <enomitch@gmail.com>
2024-07-10 14:43:02 -0500

    .

--- zshrc-specific-to-os/Darwin.zsh
+++ zshrc-specific-to-os/Darwin.zsh
@@ -53,3 +53,10 @@ test -d /Applications/Postgres.app/Contents/Versions/latest/bin && export PATH=$
 export GUILE_LOAD_PATH="/usr/local/share/guile/site/3.0"
 export GUILE_LOAD_COMPILED_PATH="/usr/local/lib/guile/3.0/site-ccache"
 export GUILE_SYSTEM_EXTENSIONS_PATH="/usr/local/lib/guile/3.0/extensions"
+
+# ------------------------------------------------
+# FREE-SPACE -------------------------------------
+# ------------------------------------------------
+function free-space() {
+  df -h / | awk 'NR==2 {printf "Total Space: %s, Free Space: %s\n", $2, $4; total=$2; free=$4; total_gb=total+0; free_gb=free+0; used_gb=total_gb-free_gb; bar="["; for (i=0; i<used_gb/total_gb*50; i++) bar=bar"#"; for (i=used_gb/total_gb*50; i<50; i++) bar=bar" "; bar=bar"]"; printf "%s\n", bar}'
+}
