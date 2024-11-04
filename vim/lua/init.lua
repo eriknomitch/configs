c881a5413ddb82c8d3ee82124bfa1df6e164abec
Erik Nomitch <enomitch@gmail.com>
2024-11-04 11:53:12 -0600

    avante

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -76,7 +76,6 @@ require("lazy").setup({
 	-- Editing support
 	{ "numToStr/Comment.nvim", opts = {} },
 	{ "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
-	{ "github/copilot.vim" },
 	{ "junegunn/vim-easy-align" },
 	{ "tpope/vim-repeat" },
 	{ "svermeulen/vim-easyclip" },
@@ -87,6 +86,55 @@ require("lazy").setup({
 		dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim" },
 	},
 	{ "samoshkin/vim-mergetool" },
+
+	-- AI
+	{ "github/copilot.vim" },
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- set this if you want to always pull the latest change
+		opts = {
+			-- add any opts here
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 
 	-- Miscellaneous
 	{ "b0o/mapx.nvim" },
