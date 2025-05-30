6c39fe14cb42dd10083f6ae3b34fb057bf5cfce9
Erik Nomitch <enomitch@gmail.com>
2025-05-30 01:16:00 -0500

    feat: add mcphub.nvim plugin with dependencies and build configuration

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -729,6 +729,16 @@ require("lazy").setup({
 		end,
 	},
 	{ "skywind3000/asyncrun.vim" }, -- Run async commands
+	{
+		"ravitemer/mcphub.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		build = "~/.asdf/shims/npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+		config = function()
+			require("mcphub").setup()
+		end,
+	},
 }, {
 	-- Lazy.nvim options
 	ui = {
